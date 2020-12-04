4f51199cafc3a68e1518afa78bc596102e7030a0
billsjchw <billsjc@126.com>
2020-12-04 11:03:22 -0500

    lab6: implement Flowgraph

--- lab6/flowgraph.c
+++ lab6/flowgraph.c
@@ -15,21 +15,69 @@
 #include "table.h"
 
 Temp_tempList FG_def(G_node n) {
-	//your code here.
-	return NULL;
+	AS_instr inst = (AS_instr) G_nodeInfo(n);
+
+	switch (inst->kind) {
+	case I_OPER:
+		return inst->u.OPER.dst;
+	case I_MOVE:
+		return inst->u.MOVE.dst;
+	case I_LABEL:
+		return NULL;
+	default:
+		assert(0);
+	}
 }
 
 Temp_tempList FG_use(G_node n) {
-	//your code here.
-	return NULL;
+	AS_instr inst = (AS_instr) G_nodeInfo(n);
+
+	switch (inst->kind) {
+	case I_OPER:
+		return inst->u.OPER.src;
+	case I_MOVE:
+		return inst->u.MOVE.src;
+	case I_LABEL:
+		return NULL;
+	default:
+		assert(0);
+	}
 }
 
 bool FG_isMove(G_node n) {
-	//your code here.
-	return TRUE;
+	AS_instr inst = (AS_instr) G_nodeInfo(n);
+
+	return inst->kind == I_MOVE;
 }
 
-G_graph FG_AssemFlowGraph(AS_instrList il, F_frame f) {
-	//your code here.
-	return NULL;
+G_graph FG_AssemFlowGraph(AS_instrList insts) {
+	G_graph graph = G_Graph();
+	G_nodeList nodes = NULL;
+	G_nodeList p = NULL;
+
+	for (; insts != NULL; insts = insts->tail)
+		G_Node(graph, insts->head);
+
+	nodes = G_nodes(graph);
+	for (p = nodes; p != NULL; p = p->tail) {
+		AS_instr inst = (AS_instr) G_nodeInfo(p->head);
+		Temp_labelList labels = inst->kind == I_OPER ? inst->u.OPER.jumps : NULL;
+		if (labels == NULL) {
+			if (p->tail != NULL)
+				G_addEdge(p, p->tail->head);
+		} else {
+			G_nodeList q = NULL;
+			for (q = nodes; q != NULL; q = q->tail) {
+				AS_instr inst = (AS_instr) G_nodeInfo(p->head);
+				if (inst->kind == I_LABEL && inst->u.LABEL.label) {
+					Temp_labelList r = NULL;
+					for (r = labels; r != NULL; r = r->tail)
+						if (r->head == inst->u.LABEL.label)
+							G_addEdge(p->head, q->head);
+				}
+			}
+		}
+	}
+
+	return graph;
 }
--- lab6/flowgraph.h
+++ lab6/flowgraph.h
@@ -5,9 +5,14 @@
 #ifndef FLOWGRAPH_H
 #define FLOWGRAPH_H
 
-Temp_tempList FG_def(G_node n);
-Temp_tempList FG_use(G_node n);
-bool FG_isMove(G_node n);
-G_graph FG_AssemFlowGraph(AS_instrList il, F_frame f);
+#include "graph.h"
+#include "temp.h"
+#include "util.h"
+#include "assem.h"
+
+Temp_tempList FG_def(G_node);
+Temp_tempList FG_use(G_node);
+bool FG_isMove(G_node);
+G_graph FG_AssemFlowGraph(AS_instrList);
 
 #endif
